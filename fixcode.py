58c5e29165244aa1d93191a388ab43d433c59355
Lucas <lw123106@gmail.com>
2026-09-08 14:52:50 -0500

    fixed code for lab 4

---
+++ fixcode.py
@@ -0,0 +1,76 @@
+#1
+#Program was missing the last ) which caused a syntax error. The corrected code is below:
+print("Welcome to Python")
+
+#2
+#Program was missing the last " which caused a syntax error. The corrected code is below:
+favorite_food = "Pizza"
+print(favorite_food)
+
+#3
+#Program was missing the _ between "student" and "name" in the print statement which caused a syntax error. The corrected code is below:
+student_name = "Alex"
+print(student_name)
+
+#4
+#Program capitilized the T in the print statement which caused a syntax error. The corrected code is below:
+temperature = 72
+print(temperature)
+
+#5
+#program was trying to add a string and integer together for a math statement. The corrected code is below:
+price = 12
+quantity = 3
+total = price * quantity
+print(total)
+
+#6
+#Program is trying to do math operations on 2 strings. The corrected code is below:
+number1 = 10
+number2 = 5
+
+total = number1 + number2
+print(total)
+
+#7
+#Program is adding price and quantity instead of multiplying them together. The corrected code is below:
+price = 15
+quantity = 4
+total = price * quantity
+print(total)
+
+#8
+#Program is multiplying length by height instead of length by width. The corrected code is below:
+length = 10
+width = 5
+area = length * width
+print(area)
+
+#9
+#Program starts with numnber = 10
+#After first operation, number = 10 + 5 = 15
+#After second operation, number = 15 * 2 = 30
+
+#10
+#Program missed the _ between "item" and "Price" in the total section and also missing the "+" between the string and integer in the print section, and also missing the str() function to convert the integer to a string. The corrected code is below:
+item_price = 10
+quantity = 4
+total = item_price * quantity
+print("Total: $ " + str(total))
+
+#Challenge
+#Program has price set to a string not an integer used in math operations. "price" in total operation is capitalized when beore it was not. Print Quantity is missing the str() function to convert the integer to a string. the print total is missing the str() function to convert the integer to a string and also the last ) in the print statement was missing. The corrected code is below:
+item = "Keyboard"
+price = 25
+quantity = 3
+total = price * quantity
+print("Item: " + item)
+print("Quantity: " + str(quantity))
+print("Total: $ " + str(total))
+
+#Reflection
+#The line helped show me where to look for the problem.
+#Not every incorrect program will show an error, some just have logic issues that dont produce a message.
+#A program that crashes with error out, one that gives the wrong answer will finish the program but show the wrong answer.
+#It is important to pay attention to the code you write so you can avoid syntax erros and spell neccassary words the same throughout the program.
+#You should look over the program to possibly spot errors in syntax or logic.
