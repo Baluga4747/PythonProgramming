794cd87f5b560b1ea0c64375cc76eeb61ad621ff
Lucas <lw123106@gmail.com>
2026-09-10 13:34:02 -0500

    agecalculator for class 9/10/26

---
+++ agecalculator.py
@@ -0,0 +1,10 @@
+init_age = input("What is your age? ")
+init_age = int(init_age)
+
+age_in_5_years = init_age + 5
+age_in_10_years = init_age + 10
+age_in_20_years = init_age + 20
+
+print(f"In 5 years, you will be {age_in_5_years} years old.")
+print(f"In 10 years, you will be {age_in_10_years} years old.")
+print(f"In 20 years, you will be {age_in_20_years} years old.")
